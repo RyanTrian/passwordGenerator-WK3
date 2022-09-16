a9020e89361c8849b43f8b13f7ec5360da611e27
Minh Truong <mptruong49@gmail.com>
2022-09-16 16:47:23 -0600

    test for generate new pw with the same rules

--- script1.js
+++ script1.js
@@ -1,15 +1,16 @@
 // Assignment Code
-
 const lower = ["a", "b" ,"c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"k" ,"l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" ,"z"];
 const upper = ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T" ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z"];
 const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 const specialChar = ["'","!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]","\"",":",";","?",">","<",",",".","/","-","="];
-
+// Global array to store values that is inside Function GeneratePassword then reuse again in the click event for an extra feature
+var varArray = [];
 
 function generatePassword() {
-
   // Ask for the password's desired length
-  let pwLength = prompt("Please indicate desired password length between 8-128 characters");
+  let pwLength = prompt(
+    "Please indicate desired password length between 8-128 characters"
+  );
   // If user click Cancel, terminate.
   if (pwLength === null) {
     return;
@@ -23,26 +24,22 @@ function generatePassword() {
   // Ask for what characters to include
   function userConfirms() {
     let letter = confirm("Will lowercase letter be included?");
-    // console.log(letter);
     let upperCase = confirm("Will upppercase letter be included?");
-    // console.log(upperCase);
     let number = confirm("Will number be included?");
-    // console.log(number);
     let specialChar = confirm("Will special characters be included?");
     // console.log(specialChar);
     if (!(letter || upperCase || number || specialChar)) {
-      alert("Must confirm at least one")
+      alert("Must confirm at least one");
       return userConfirms();
     }
-    return [letter, upperCase, number, specialChar]
+    return [letter, upperCase, number, specialChar];
   }
   // Store each Boolean elements inside new variables
   let userBoolean = userConfirms();
-  console.log(userBoolean);
   userLetter = userBoolean[0];
-  userUpper = userBoolean [1];
-  userNumber = userBoolean [2];
-  userSpecial = userBoolean [3];
+  userUpper = userBoolean[1];
+  userNumber = userBoolean[2];
+  userSpecial = userBoolean[3];
 
   // Create an array that nested the array constants if Boolean value is true
   var userChoice = new Array();
@@ -57,37 +54,64 @@ function generatePassword() {
   }
   if (userNumber) {
     userChoice[n] = number;
-    n++
+    n++;
   }
   if (userSpecial) {
     userChoice[n] = specialChar;
   }
-  // console.log(userChoice);
 
   // Generate Password
   let generatedPassword = "";
   for (let i = 0; i < pwLength; i++) {
-    let randomIndex = Math.floor(Math.random() * (n+1));
-    let randomIndex2 = Math.floor(Math.random() * userChoice[randomIndex].length);
+    let randomIndex = Math.floor(Math.random() * (n + 1));
+    let randomIndex2 = Math.floor(
+      Math.random() * userChoice[randomIndex].length
+    );
     let randomChar = userChoice[randomIndex][randomIndex2];
     generatedPassword = generatedPassword.concat(randomChar);
-    // console.log(generatedPassword);
   }
-  return generatedPassword;
+  var varArray = [generatedPassword, userChoice, n, pwLength];
+  console.log(varArray);
+  return varArray;
 }
-
+console.log(typeof varArray[0]);
 //Reference to Generate Button
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+var passwordText = document.querySelector("#password");
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  if (passwordText.value === "") {
+    generatePassword();
+  }
+  let password = varArray[0].toString();
 
   passwordText.value = password;
-
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button *Extra: generate another pw with the same rules
+generateBtn.addEventListener("click", function () {
+  if (passwordText.value !== "") {
+    let wantPw = confirm("Do you want to generate another one?");
+    if (wantPw === true) {
+      let userChoice = varArray[1];
+      let n = varArray[2];
+      let pwLength = varArray[3];
+      let generatedPassword = "";
+      for (let i = 0; i < pwLength; i++) {
+        let randomIndex = Math.floor(Math.random() * (n + 1));
+        let randomIndex2 = Math.floor(
+          Math.random() * userChoice[randomIndex].length
+        );
+        let randomChar = userChoice[randomIndex][randomIndex2];
+        generatedPassword = generatedPassword.concat(randomChar);
+      }
+      varArray[0] = generatedPassword;
+      writePassword();
+    }
+  } 
+  else {
+    writePassword();
+  }
+});
 
