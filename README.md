1954b92b6be9eb426dc5857e5733b234d11c3976
Minh Truong <mptruong49@gmail.com>
2022-09-17 17:23:10 -0600

    README

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# passwordGenerator-WK3
-BootCamp Week 3 Challenge. Write new JS code so that the final project can generate a random password that follow certain conditions
+# PasswordGenerator-WK3
+
+<h2 align="center"><img width="550" alt="Static" src="https://user-images.githubusercontent.com/82792300/190870686-eb3ace1c-9cdc-415a-ab4c-e9767fc4aeb0.png"></h2>
+
+## TLDR 👀
+
+A password generator coded with JS. Generate a password based on user's answers to prompt and confirm boxes. 
+
+**\*Extra\* - I went beyond the challenge**: 
+- After the first password, user can click the button again for another password without answering all the prompt and confirm boxes again.
+- The page work intuitively, means the cancel button do what the cancel button does, so that the boxes stop pestering users to answer while keeping some constraints on user's input.
+
+[You can visit the deployed project here](https://ryantrian.github.io/passwordGenerator-WK3/)
+
+## Demo
+
+<img width="1207" alt="Demo" src="https://user-images.githubusercontent.com/82792300/190870684-96ef20b2-64bf-45c3-9c4f-d97f6bbb69f4.gif">
+
+## Usage
+
+1. Click the "Generate Password" button
+2. Answer how long the password should be
+3. Confirm or Cancel four boxes that ask if lower-case, upper-case, number or special characters should be included
+4. Click the button again, then confirm the box If want another password.
+5. Reset the page if want to apply different rules to the password.
+
+
+## Challenges 🤔
+
+- A quite common solution is pool whichever characters array that user want to a new array and math.random that new array. I think with that solution, it could create a password which has one type of character more than the other, ex. too many lower case letter. 
+  - Therefore, I used **nested arrays** instead. A new array that will contain the character-array users want, then have math.random pick one of the nested array, and math.random again to pick an element inside that array. It required some if logic to create the array that hold user's choices. 
+- After doing multiple tests, I didn't like that to create a second password I have to answers all the questions again. So I added an If logic inside the "Generate Password" button's click event. User just need to click the button again and confirms they want another password, a different password will generate with the same rules from the previous one. 
+
+  - <img width="1207" alt="Generate-another" src="https://user-images.githubusercontent.com/82792300/190870685-f4fb47c4-8c9f-46b7-b068-44a88d6f6eed.png">
+
+- Finally, I ran into a major issue due to the extra feature I want to add. It was due to variable scoping. However, thanks to this problem, I now had some practice with global, local scoping, and debugging too.
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
--- script.js
+++ script.js
@@ -1,94 +1,94 @@
-// Assignment Code
+// // Assignment Code
 
-const lower = ["a", "b" ,"c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"k" ,"l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" ,"z"];
-const upper = ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T" ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z"];
-const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-const specialChar = ["'","!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]","\"",":",";","?",">","<",",",".","/","-","="];
+// const lower = ["a", "b" ,"c" ,"d" ,"e" ,"f" ,"g" ,"h" ,"i" ,"j" ,"k" ,"l" ,"m" ,"n" ,"o" ,"p" ,"q" ,"r" ,"s" ,"t" ,"u" ,"v" ,"w" ,"x" ,"y" ,"z"];
+// const upper = ["A" ,"B" ,"C" ,"D" ,"E" ,"F" ,"G" ,"H" ,"I" ,"J" ,"K" ,"L" ,"M" ,"N" ,"O" ,"P" ,"Q" ,"R" ,"S" ,"T" ,"U" ,"V" ,"W" ,"X" ,"Y" ,"Z"];
+// const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// const specialChar = ["'","!","@","#","$","%","^","&","*","(",")","_","+","~","`","|","}","{","[","]","\"",":",";","?",">","<",",",".","/","-","="];
 
 
-function generatePassword() {
+// function generatePassword() {
 
-  // Ask for the password's desired length
-  let pwLength = prompt("Please indicate desired password length between 8-128 characters");
-  // If user click Cancel, terminate.
-  if (pwLength === null) {
-    return;
-  }
-  // If the pwLength is out of range, run generatePassword again
-  else if (pwLength < 8 || pwLength > 128) {
-    alert("Please enter a value in 8-128");
-    return generatePassword();
-  }
+//   // Ask for the password's desired length
+//   let pwLength = prompt("Please indicate desired password length between 8-128 characters");
+//   // If user click Cancel, terminate.
+//   if (pwLength === null) {
+//     return;
+//   }
+//   // If the pwLength is out of range, run generatePassword again
+//   else if (pwLength < 8 || pwLength > 128) {
+//     alert("Please enter a value in 8-128");
+//     return generatePassword();
+//   }
 
-  // Ask for what characters to include
-  function userConfirms() {
-    let letter = confirm("Will lowercase letter be included?");
-    // console.log(letter);
-    let upperCase = confirm("Will upppercase letter be included?");
-    // console.log(upperCase);
-    let number = confirm("Will number be included?");
-    // console.log(number);
-    let specialChar = confirm("Will special characters be included?");
-    // console.log(specialChar);
-    // If user did not choose any of the above, loop back to userConfirms()
-    if (!(letter || upperCase || number || specialChar)) {
-      alert("Must confirm at least one")
-      return userConfirms();
-    }
-    return [letter, upperCase, number, specialChar]
-  }
-  // Store each Boolean elements inside new variables
-  let userBoolean = userConfirms();
-  console.log(userBoolean);
-  userLetter = userBoolean[0];
-  userUpper = userBoolean [1];
-  userNumber = userBoolean [2];
-  userSpecial = userBoolean [3];
+//   // Ask for what characters to include
+//   function userConfirms() {
+//     let letter = confirm("Will lowercase letter be included?");
+//     // console.log(letter);
+//     let upperCase = confirm("Will upppercase letter be included?");
+//     // console.log(upperCase);
+//     let number = confirm("Will number be included?");
+//     // console.log(number);
+//     let specialChar = confirm("Will special characters be included?");
+//     // console.log(specialChar);
+//     // If user did not choose any of the above, loop back to userConfirms()
+//     if (!(letter || upperCase || number || specialChar)) {
+//       alert("Must confirm at least one")
+//       return userConfirms();
+//     }
+//     return [letter, upperCase, number, specialChar]
+//   }
+//   // Store each Boolean elements inside new variables
+//   let userBoolean = userConfirms();
+//   console.log(userBoolean);
+//   userLetter = userBoolean[0];
+//   userUpper = userBoolean [1];
+//   userNumber = userBoolean [2];
+//   userSpecial = userBoolean [3];
 
-  // Create an array that nested the array constants if Boolean value is true
-  var userChoice = new Array();
-  var n = 0;
-  if (userLetter) {
-    userChoice[n] = lower;
-    n++;
-  }
-  if (userUpper) {
-    userChoice[n] = upper;
-    n++;
-  }
-  if (userNumber) {
-    userChoice[n] = number;
-    n++
-  }
-  if (userSpecial) {
-    userChoice[n] = specialChar;
-  }
-  // console.log(userChoice);
+//   // Create an array that nested the array constants if Boolean value is true
+//   var userChoice = new Array();
+//   var n = 0;
+//   if (userLetter) {
+//     userChoice[n] = lower;
+//     n++;
+//   }
+//   if (userUpper) {
+//     userChoice[n] = upper;
+//     n++;
+//   }
+//   if (userNumber) {
+//     userChoice[n] = number;
+//     n++
+//   }
+//   if (userSpecial) {
+//     userChoice[n] = specialChar;
+//   }
+//   // console.log(userChoice);
 
-  // Generate Password
-  let generatedPassword = "";
-  for (let i = 0; i < pwLength; i++) {
-    let randomIndex = Math.floor(Math.random() * (n+1));
-    let randomIndex2 = Math.floor(Math.random() * userChoice[randomIndex].length);
-    let randomChar = userChoice[randomIndex][randomIndex2];
-    generatedPassword = generatedPassword.concat(randomChar);
-    // console.log(generatedPassword);
-  }
-  return generatedPassword;
-}
+//   // Generate Password
+//   let generatedPassword = "";
+//   for (let i = 0; i < pwLength; i++) {
+//     let randomIndex = Math.floor(Math.random() * (n+1));
+//     let randomIndex2 = Math.floor(Math.random() * userChoice[randomIndex].length);
+//     let randomChar = userChoice[randomIndex][randomIndex2];
+//     generatedPassword = generatedPassword.concat(randomChar);
+//     // console.log(generatedPassword);
+//   }
+//   return generatedPassword;
+// }
 
-//Reference to Generate Button
-var generateBtn = document.querySelector("#generate");
+// //Reference to Generate Button
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// // Write password to the #password input
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
 
